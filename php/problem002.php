1f054264c8366d08b1d6827e7898e13c8e6d89f7
Dave Albert <dave.albert@publicisdhealthcare.ie>
2013-06-20 13:41:09 +0100

    Answer P2

--- php/problem002.php
+++ php/problem002.php
@@ -6,3 +6,18 @@
 
   By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 */
+
+  $a = 1;
+  $b = 2;
+  $sum = 0;
+
+  while($b < 4000000){
+    if ($b % 2 === 0) {
+      $sum += $b;
+    }
+    echo "b: $b, a: $a, sum: $sum\n";
+    $tmp = $a+$b;
+    $a = $b;
+    $b = $tmp;
+  }
+  echo "\nsum: $sum\n\n";
