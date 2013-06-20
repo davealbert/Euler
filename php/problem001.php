325f8c8547f35806eb7bf39da162feca0d89f8d9
Dave Albert <dave.albert@publicisdhealthcare.ie>
2013-06-20 09:29:20 +0100

    problem 1 finished, 2 added

--- php/problem001.php
+++ php/problem001.php
@@ -6,3 +6,11 @@
   Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
+  $limit = 1000;
+  $sum = 0;
+  for ($i=1; $i < $limit; $i++) {
+    if (($i % 3 === 0) || ($i % 5 === 0)) {
+      $sum += $i;
+    }
+  }
+  echo $sum;
